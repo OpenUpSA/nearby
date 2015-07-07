bdba824e6a05cc158c9f5baa56e21ee7c9e1f150
Greg Kempe <greg@kempe.net>
2015-07-07 13:26:47 +0200

    Put our styling first.

--- nearby/settings.py
+++ nearby/settings.py
@@ -124,8 +124,8 @@ PYSCSS_LOAD_PATHS = [
 PIPELINE_CSS = {
     'css': {
         'source_filenames': (
-            'bower_components/fontawesome/css/font-awesome.css',
             'bower_components/code4sa-styles/css/code4sa-custom-bootstrap.css',
+            'bower_components/fontawesome/css/font-awesome.css',
             'stylesheets/app.scss',
         ),
         'output_filename': 'app.css',
