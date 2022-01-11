0c54e9bcef9a7059d055ad38cc21d076101e0fac
JD Bothma <jbothma@gmail.com>
2022-01-11 17:58:59 +0200

    Move away from manifest storage

--- nearby/settings.py
+++ nearby/settings.py
@@ -222,7 +222,7 @@ PIPELINE = {
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 # Logging
