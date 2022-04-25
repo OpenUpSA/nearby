cd46ab3996daad4f14faef0d5c6346fc599cf744
LucaBazzea <luca.bazzea@gmail.com>
2022-04-25 14:54:06 +0200

    Update to CouncillorContactInfo model test

--- nearby/tests/test_models.py
+++ nearby/tests/test_models.py
@@ -5,12 +5,11 @@ from nearby.models import CouncillorContactInfo
 class TestModels(TestCase):
 
     def test_model_str(self):
-        ward_id = CouncillorContactInfo.objects.create(ward_id="314159")
-        councillor = CouncillorContactInfo.objects.create(councillor="Jim Testingson")
-        phone = CouncillorContactInfo.objects.create(phone="123 456 7890")
-        email = CouncillorContactInfo.objects.create(email="jim@gov.za")
+        ward0 = CouncillorContactInfo.objects.create(
+            ward_id="314159",
+            councillor="Jim Testingson",
+            phone="123 456 7890",
+            email="jim@gov.za"
+        )
 
-        self.assertEqual(str(ward_id), "314159")
-        self.assertEqual(str(councillor), "Jim Testingson")
-        self.assertEqual(str(phone), "123 456 7890")
-        self.assertEqual(str(email), "jim@gov.za")
+        self.assertEqual(str(ward0), "314159")
