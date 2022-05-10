fd155a89757828be8bcddd5d8976ade910394d0c
LucaBazzea <luca.bazzea@gmail.com>
2022-05-10 16:35:04 +0200

    Test for CouncillorContactInfo data in the API

--- nearby/tests/test_models.py
+++ nearby/tests/test_models.py
@@ -1,15 +1,29 @@
 from django.test import TestCase
-from nearby.models import CouncillorContactInfo
+import unittest
+import requests
 
 
 class TestModels(TestCase):
 
-    def test_model_str(self):
-        ward0 = CouncillorContactInfo.objects.create(
-            ward_id="314159",
-            councillor="Jim Testingson",
-            phone="123 456 7890",
-            email="jim@gov.za"
-        )
+    def test_contact_info_present(self):
+        response = requests.get("http://localhost:8000/councillor/ward-19100064.json")
 
-        self.assertEqual(str(ward0), "314159")
+        if response.status_code == 200:
+            contact_info = response.json()["councillor"]["custom_contact_details"]
+
+            self.assertEqual(r'contact_info, {"phone": "062-873-2894", "email": "aimeek.da@gmail.com"}')
+
+        else:
+            print(f"status code: {response.status_code}")
+
+
+    def test_contact_info_missing(self):
+        response = requests.get("http://localhost:8000/councillor/ward-19100062.json")
+
+        if response.status_code == 200:
+            contact_info = response.json()["councillor"]["custom_contact_details"]
+
+            self.assertEqual(r'contact_info, {}')
+
+        else:
+            print(f"status code: {response.status_code}")
